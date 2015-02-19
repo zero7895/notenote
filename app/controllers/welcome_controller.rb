ccf42dfb10618b46dae669d3ff732917c9df4ed4
Nate Chen <natechen.ntu@gmail.com>
2015-02-19 20:01:45 +0800

    Add note views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,27 +2,4 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def show
-	end
-
-	def create
-	end
-
-	def edit
-	end
-
-	def update
-	end
-
-	def destroy
-	end
-
-	private
-
-	def find_note
-	end
-
-	def note_params
-	end
-	
 end
